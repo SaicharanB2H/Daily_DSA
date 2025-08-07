6d6a92d627e9341fce908df9e727b02db6515373
saicharanb2h <saicharanb3h@gmail.com>
2025-08-07 22:58:21 +0530

    completed some basic questions on arrays.solved leetcode problem

--- arrays.cpp
+++ arrays.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main(){
 
+    int arr[] = {1,2,1,2,3,4,4};
+    set<int> st;
+    int n = sizeof(arr)/sizeof(arr[0]);
 
+    for(int i=0;i<n;i++){
+        st.insert(arr[i]);
+    }
+    int index =0;
+    for(auto it:st){
+        arr[index] = it;
+        index++;
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
     
 }
