114e0182e96e099a50c47c281b34fd255dfb55b3
saicharanb2h <saicharanb3h@gmail.com>
2025-08-11 00:19:23 +0530

    Day 7 operations on arrays

--- arrays.cpp
+++ arrays.cpp
@@ -4,18 +4,36 @@ using namespace std;
 
 int main(){
 
-    int arr[] = {1,2,1,2,3,4,4};
-    set<int> st;
-    int n = sizeof(arr)/sizeof(arr[0]);
+    // int arr[] = {1,2,1,2,3,4,4};
+    // set<int> st;
+    // int n = sizeof(arr)/sizeof(arr[0]);
 
-    for(int i=0;i<n;i++){
-        st.insert(arr[i]);
+    // for(int i=0;i<n;i++){
+    //     st.insert(arr[i]);
+    // }
+    // int index =0;
+    // for(auto it:st){
+    //     arr[index] = it;
+    //     index++;
+    // }
+
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i]<<" ";
+    // }
+
+    int arr[] {1,2,3,4,5};
+
+    int temp = arr[0];
+    
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=1;i<n;i++){
+        arr[i-1] = arr[i];
+        
     }
-    int index =0;
-    for(auto it:st){
-        arr[index] = it;
-        index++;
-    }
+
+    
+    arr[n-1] = temp;
+
 
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
